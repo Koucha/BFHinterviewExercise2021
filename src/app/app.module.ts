2378a6ae294826b8220c11ae5bef17f2a5a21f08
Koucha <koucha@koucha.ch>
2021-05-11 12:58:54 +0200

    Cleanup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Task1cardComponent } from './task1card/task1card.component';
-import {MaterialModule} from './material/material.module';
+import { MaterialModule } from './material/material.module';
 import { Task3validatorPipe } from './task3validator.pipe';
 
 @NgModule({
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const MaterialComponents = [
   MatInputModule,
-  MatTabsModule
+  MatTabsModule,
+  MatFormFieldModule
 ];
 
 @NgModule({
